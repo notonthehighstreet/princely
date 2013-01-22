877c1dbce26e9a11839b3cb9749848a903f7cdd8
Douglas Roper <douglasroper@notonthehighstreet.com>
2013-01-22 12:19:45 +0000

    dont use asset_file_path

--- lib/princely/pdf_helper.rb
+++ lib/princely/pdf_helper.rb
@@ -35,7 +35,7 @@ module PdfHelper
 
     # Make all paths relative, on disk paths...
     html_string.gsub!(".com:/",".com/") # strip out bad attachment_fu URLs
-    html_string.gsub!( /src=["']+([^:]+?)["']/i ) { |m| "src=\"#{asset_file_path($1)}\"" } # re-route absolute paths
+    html_string.gsub!( /src=["']+([^:]+?)["']/i, "src=\"#{Rails.public_path}/\\1\"")
 
     # Remove asset ids on images with a regex
     html_string.gsub!( /src=["'](\S+\?\d*)["']/i ) { |m| 'src="' + $1.split('?').first + '"' }
